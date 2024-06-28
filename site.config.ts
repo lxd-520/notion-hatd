357d1e0152e6dfee212bd516628f2d0e477501ee
lxd-520 <106861392+lxd-520@users.noreply.github.com>
2024-06-28 17:47:08 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Next.js Notion hatd',
+  domain: 'notion-hatd.lxd-520.it',
+  author: 'admin',
 
   // open graph metadata (optional)
   //description: 'Example Next.js Notion Starter Kit Site',
